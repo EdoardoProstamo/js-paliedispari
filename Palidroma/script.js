1b3d38d1926c910cd475eae90b50be306ce4e60b
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-05 14:55:03 +0100

    feat: scaffolding Palidroma

---
+++ Palidroma/script.js
@@ -0,0 +1,16 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+const userWord = parseInt (prompt('Inserisci una parola'));
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+
+
+
+
+
+
+
+
+
