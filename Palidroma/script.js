aaf199d574e7bb0dce6fd08e0ef8284c5b9fef50
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-05 15:09:00 +0100

    feat: scrittura funzione

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -1,12 +1,20 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
-const userWord = parseInt (prompt('Inserisci una parola'));
+// const userWord = parseInt (prompt('Inserisci una parola'));
 // Creare una funzione per capire se la parola inserita è palindroma
 
 
+const userWord = 'anna';
+if(true) {
+    console.log('Sono una parola palindroma');
+}else {
+    console.log('Non sono una parola palindroma');
+}
 
-
-
+//determina se una parola è palindroma
+function isPalindrome(input) {
+    return false;
+}
 
 
 
