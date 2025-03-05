6dbbb56a02534f8aef16c8436162d51f8edc25ef
EdoardoProstamo <edoardoprostamo.19@libero.it>
2025-03-05 15:26:25 +0100

    feat: maiuscole e array

--- Palidroma/script.js
+++ Palidroma/script.js
@@ -1,10 +1,10 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola
-// const userWord = parseInt (prompt('Inserisci una parola'));
+const userWord = parseInt (prompt('Inserisci una parola'));
 // Creare una funzione per capire se la parola inserita è palindroma
 
 
-const userWord = 'anna';
+const isWordPalindrome = isPalindrome(userWord);
 if(true) {
     console.log('Sono una parola palindroma');
 }else {
@@ -13,6 +13,13 @@ if(true) {
 
 //determina se una parola è palindroma
 function isPalindrome(input) {
+    
+    //faccio in modo che le maiuscole diventino minuscole, non venendo quindi considerate nel conteggio
+    const word = input.toLowerCase();
+
+    //trasformo la parola in un array(perchè altrimenti non posso usare il reverse) e poi la faccio ritornare una stringa
+    const reverse = word.split('').reverse().join();
+
     return false;
 }
 
